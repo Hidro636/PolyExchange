280d8d2767103a5f4222afb19a76c65ea826670e
unknown <liblabs-user@35216B02-201041.ad.calpoly.edu>
2015-12-08 14:14:48 -0800

    stuff

--- login.js
+++ login.js
@@ -1,27 +1,48 @@
-if(Meteor.isClient) {
-    Meteor.startup(function() {
+if (Meteor.isClient) {
+    Meteor.startup(function () {
         Session.setDefault("showLogin", true);
+        Session.setDefault("test", false);
+        Session.setDefault("error", false);
+    });
+
+    Template.body.helpers({
+        test: function () {
+            return Session.get('test');
+        }
     });
 
     Template.loginWindow.helpers({
-        showLogin: function() {
+        showLogin: function () {
             return Session.get("showLogin");
+        },
+        error: function(){
+            return Session.get('error');
         }
     });
 
     Template.loginWindow.events({
-        "submit #loginForm": function(event, target) {
-            event.preventDefault();
-        },
-        "click #loginTab": function(e, t) {
+        "click #loginTab": function (e) {
             e.preventDefault();
 
             Session.set("showLogin", true);
         },
-        "click #createAccountTab": function(e, t){
+        "click #createAccountTab": function (e) {
             e.preventDefault();
-
+            Session.set('error', false);
             Session.set("showLogin", false);
+        }
+    });
+
+    Template.loginForm.events({
+        "submit #loginForm": function (e) {
+            e.preventDefault();
+            var email = e.target.emailTextBox.value;
+            var password = e.target.passwordTextBox.value;
+
+            if(email == '' || password == ''){
+                $('#errorLabel').text("A field was left blank!");
+            }
+
         }
     });
 }
